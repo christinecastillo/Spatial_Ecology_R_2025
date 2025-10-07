652143ae0403b59aa2e4257efd5361bdb3c08695
Christine Castillo <chris.castillo0305@gmail.com>
2025-10-07 17:45:11 +0200

    Create 01_my_first_R_script.R

---
+++ Code/01_my_first_R_script.R
@@ -0,0 +1,20 @@
+# This is a comment and will help humans understand 
+
+2+3
+[1] 5
+naza <- 2+3
+marco <- 7+4
+naza+marco
+[1] 16
+vale <- c(5, 10,13,20,30)
+vale
+[1]  5 10 13 20 30
+elia <- c(2,5,10,40,70)
+elia
+[1]  2  5 10 40 70
+plot(elia, vale)
+plot(elia, vale, pch=19)
+plot(elia, vale, pch=19, col="red")
+plot(elia, vale, pch=19, col="red", cex=2)
+plot(elia, vale, pch=19, col="red", cex=.7)
+plot(elia, vale, pch=19, col="red", cex=2, xlab="Forest biomas", ylab="Leopard abundance")
