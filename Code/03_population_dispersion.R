d864d404ece71394c6e6fbc8118244514f495186
Christine Castillo <chris.castillo0305@gmail.com>
2025-10-17 10:48:10 +0200

    Update 03_population_dispersion.R

--- Code/03_population_dispersion.R
+++ Code/03_population_dispersion.R
@@ -14,4 +14,31 @@ rana
 
 plot(rana)
 
+# Occurrences
 rana$Occurrence
+
+# Selection of presences
+pres <- rana[rana$Occurrence==1]
+plot(pres)
+
+# Exercise: select all absences
+abse <- rana[rana$Occurrence==0]
+# or
+abse <- rana[rana$Occurrence!=1]
+plot(abse)
+
+abse$Occurrence
+pres$Occurrence
+
+# Exercise: plot the presences with a color together with the absences with another color
+plot(abse, col="#66CD00") # #66CD00
+points(pres, col="#FF1493") #deeppink #FF1493
+
+# I cannot use the plot because it will overwrite
+
+# Exercise: do the same in a multiframe with the two sets: pres on top 
+par(mfrow=c(2,1)) 
+plot(pres)
+plot(abse)
+
+#xlim and ylim to change the x and y to be the same on the graphs
