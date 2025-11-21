6d6451c5c34ca197d2206602e043b210ece015e1
Christine Castillo <chris.castillo0305@gmail.com>
2025-11-21 10:49:39 +0100

    Create 10_Remote_Sensing_Visualization.R

---
+++ Code/10_Remote_Sensing_Visualization.R
@@ -0,0 +1,8 @@
+# Code to visualize remote sensing data
+
+# Zenodo set: https://zenodo.org/records/15645465
+# install.packages("imageRy")
+library(imageRy)
+
+im.list() # Muestra todos los archivos que hay dentro  #Zenodo es una open source 
+
