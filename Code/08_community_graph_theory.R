bc58562d6dae2aa19c4f8838cd4fa7dfd906e6c5
Christine Castillo <chris.castillo0305@gmail.com>
2025-11-21 09:34:59 +0100

    Update 08_community_graph_theory.R

--- Code/08_community_graph_theory.R
+++ Code/08_community_graph_theory.R
@@ -8,3 +8,14 @@ predator <- c("Zooplankton", "Small Fish", "Large Fish", "Bird", "Bird")
 prey <- c("Algae", "Zooplankton", "Small Fish", "Small Fish", "Large Fish")   # Los ordenamos de acuerdo a cadena alimentaria ex. Bird eat Small and Large Fish
 
 interactions <- data.frame(predator, prey)
+interactions
+
+# Create graph
+g <- graph_from_data_frame(interactions, vertices = species, directed = T)
+plot(g)
+
+#set.seed()
+
+set.seed(42)
+g <- graph_from_data_frame(interactions, vertices = species, directed = T)
+plot(g)
